5fa81366b492f7b481c0e68c5e209edd8e431a99
NikolaiOD <110899590+NikolaiOD@users.noreply.github.com>
2023-01-10 23:22:10 +0200

    2 task is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+// Инициализация плеера
+import Player from '@vimeo/player';
+// Инициализация lodash
+import throttle from "lodash.throttle";
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+
+const player = new Player(vimeoPlayer);
+
+player.on('timeupdate', throttle(playbackTime, 1000));
+
+function playbackTime(event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+const getPlaybackTime = (
+    localStorage.getItem('videoplayer-current-time') || 0
+);
+
+player.setCurrentTime(getPlaybackTime);
